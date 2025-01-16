84e3151f3547b430eefa1553d8055b7cd6d519ba
PatricioPonce28 <geanotoponce95@gmail.com>
2025-01-16 08:36:38 -0500

    Commit

--- src/main/java/org/example/form1.java
+++ src/main/java/org/example/form1.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class form1 {
     private JTextField textField1;
@@ -8,7 +10,21 @@ public class form1 {
     private JButton btnIngresar;
     private JComboBox comboBox1;
     private JPanel formLogin;
+    private JPasswordField passwordField1;
     private JFrame frame;
+
+    public form1() {
+        btnIngresar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String usuario = textField1.getText();
+                String contra = new String(textField2.getPassword())
+
+
+            }
+        });
+    }
 
     public void ventanaLogin(){
         frame = new JFrame("Login");
